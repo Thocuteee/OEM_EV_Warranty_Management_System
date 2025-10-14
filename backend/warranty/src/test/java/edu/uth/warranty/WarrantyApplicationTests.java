1e2bba45b6cdf5d34013a45c0f93b97141751ab3
Tho <thoch3765@ut.edu.vn>
2025-10-15 04:59:08 +0700

    restore tree project

---
+++ backend/warranty/src/test/java/edu/uth/warranty/WarrantyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.warranty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarrantyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
