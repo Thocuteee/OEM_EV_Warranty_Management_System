6df9dd2898d46b353df03b7c1994380c97d7a8a6
dduy26 <duyda8739@ut.edu.vn>
2025-10-28 17:46:40 +0700

    Model update

--- backend/warranty/src/main/java/edu/uth/warranty/repository/InvoiceRepository.java
+++ backend/warranty/src/main/java/edu/uth/warranty/repository/InvoiceRepository.java
@@ -1,19 +1,26 @@
 package edu.uth.warranty.repository;
 
-import edu.uth.warranty.model.Invoice;
-import edu.uth.warranty.model.ServiceCenter;
-import edu.uth.warranty.model.WarrantyClaim;
-
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import edu.uth.warranty.model.Invoice;
+
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long>{
-    
+    Optional<Invoice> findByInvoice_id(Long invoiceId);
 
+    List<Invoice> findByCenter_id(Long centerId);
 
+    List<Invoice> findByClaim_id(Long claimId);
 
+    List<Invoice> findByLocation(String location);
+
+    List<Invoice> findByQuantity(Integer quantity);
+
+    List<Invoice> findByMinStockLevel(Integer minStockLevel);
+
+    List<Invoice> findByPaymentsStatus(String paymentsStatus);
 }
