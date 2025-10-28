b623ff74afb94e0abd4b068b146afff52f0d0de4
dduy26 <duyda8739@ut.edu.vn>
2025-10-28 19:17:31 +0700

    update 5 model warranty

--- backend/warranty/src/main/java/edu/uth/warranty/repository/CustomerRepository.java
+++ backend/warranty/src/main/java/edu/uth/warranty/repository/CustomerRepository.java
@@ -1,3 +1,6 @@
+
+
+
 public interface CustomerRepository {
 
 }
--- backend/warranty/src/main/java/edu/uth/warranty/repository/PartRepository.java
+++ backend/warranty/src/main/java/edu/uth/warranty/repository/PartRepository.java
@@ -1,3 +1,20 @@
+package edu.uth.warranty.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import edu.uth.warranty.model.Part;
+
+@Repository
 public interface PartRepository {
+    Optional<Part> findByPartId(String partId);
+
+    Optional<Part> findByPartNumber(String partNumber);
+
+    List<Part> findByName(String name);
+    
+    List<Part> findByPrice(java.math.BigDecimal price);
 
 }
