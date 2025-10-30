5443fa1be75535835b9fc25c1e0add3b071434ea
Tho <thoch3765@ut.edu.vn>
2025-10-30 15:11:09 +0700

    da xong 4 table service

--- backend/warranty/src/main/java/edu/uth/warranty/service/IVehicleService.java
+++ backend/warranty/src/main/java/edu/uth/warranty/service/IVehicleService.java
@@ -1,5 +1,30 @@
 package edu.uth.warranty.service;
 
-public interface IVehicleService {
+import edu.uth.warranty.model.Vehicle;
+import edu.uth.warranty.model.Customer;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IVehicleService {
+    List<Vehicle> getAllVehicle();
+
+    Optional<Vehicle> getAllById(Long id);
+
+    Vehicle saveVehicle(Vehicle vehicle);
+
+    void deleteVehicle(Long id);
+
+    Optional<Vehicle> getVehicleByVIN(String VIN);
+
+    boolean isVINUnique(String VIN);
+
+    List<Vehicle> getVehiclesByCustomer(Customer customer);
+
+    Optional<Vehicle> getVehicleByModel(String model);
+
+    List<Vehicle> getVehiclesByYear(String year);
 }
