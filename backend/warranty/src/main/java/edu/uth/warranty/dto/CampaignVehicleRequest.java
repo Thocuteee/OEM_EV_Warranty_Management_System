f8134a31918e81037eefb2b073ce68d572667e58
vovanvinhquang-code <quangvvv7552@ut.edu.vn>
2025-11-15 22:07:37 +0700

    CampaignVehicleRequest Update

--- backend/warranty/src/main/java/edu/uth/warranty/dto/CampaignVehicleRequest.java
+++ backend/warranty/src/main/java/edu/uth/warranty/dto/CampaignVehicleRequest.java
@@ -1,10 +1,10 @@
 package edu.uth.warranty.dto;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
