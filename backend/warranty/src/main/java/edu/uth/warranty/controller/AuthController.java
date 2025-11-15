6ee9e9458123a0fb58eb9fd694070ca5e4b7f62b
dduy26 <duyda8739@ut.edu.vn>
2025-11-15 16:38:34 +0700

    update AuthController

--- backend/warranty/src/main/java/edu/uth/warranty/controller/AuthController.java
+++ backend/warranty/src/main/java/edu/uth/warranty/controller/AuthController.java
@@ -1,19 +1,21 @@
 package edu.uth.warranty.controller;
 
-import edu.uth.warranty.dto.LoginResponse;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import edu.uth.warranty.dto.LoginRequest;
+import edu.uth.warranty.dto.LoginResponse;
 import edu.uth.warranty.dto.MessageResponse;
 import edu.uth.warranty.dto.UserRequest;
 import edu.uth.warranty.model.User;
 import edu.uth.warranty.service.IUserService;
-import jakarta.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
