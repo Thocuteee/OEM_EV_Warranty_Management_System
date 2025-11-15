d5ced2052199d8de99ca7f1407f3d4b8185c0fbe
dduy26 <duyda8739@ut.edu.vn>
2025-11-15 22:37:37 +0700

    update common 2 file

---
+++ backend/warranty/src/main/java/edu/uth/warranty/common/Role.java
@@ -0,0 +1,8 @@
+package edu.uth.warranty.common;
+
+public enum Role {
+    SC_Staff,         // Nhân viên Trung tâm Dịch vụ
+    SC_Technician,   // Kỹ thuật viên Trung tâm Dịch vụ
+    EVM_Staff,       // Nhân viên Trung tâm Dịch vụ EVM
+    Admin            // Quản trị viên
+}
